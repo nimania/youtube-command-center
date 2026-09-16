6eae33e4225dc2bbb9ca608de6aeaa2cc6a0211f
nimania <60144238+nimania@users.noreply.github.com>
2026-09-16 12:06:06 +0330

    Configure Google OAuth client for live YouTube analytics

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.YT_DASHBOARD_CONFIG = {
-  googleClientId: "",
+  googleClientId: "1088888982495-3rtd3cdufll0masasvdo4af0aouv1s6s.apps.googleusercontent.com",
   channelHandle: "@nimaafsharnaderi",
   locale: "fa-IR",
   timezone: "Asia/Tehran"
